579ed15291df7a44a71ebb88b0357eb8794af7ee
Khurram Virani <khurram.virani@softgravity.com>
2011-03-23 10:19:33 -0400

    trial5 fixes

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,7 +2,7 @@ class OffersController < ApplicationController
 
   layout 'offers'
 
-  caches_page :trial1, :trial2, :trial3, :trial4
+  caches_page :trial1, :trial2, :trial3, :trial4, :trial5
 
 
 end
