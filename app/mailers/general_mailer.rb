a2c0609eb91da1793ef76377d7ebb1d5320e5875
Khurram Virani <kvirani@gmail.com>
2012-03-18 14:56:10 -0400

    remove tyler from mailers

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -3,11 +3,11 @@ class GeneralMailer < ActionMailer::Base
 
   def signup_request(signup)
     @signup = signup
-    mail(:to => ["kvirani@span6.com", "tyler@span6.com"], :subject => "Span6 Demo Request")
+    mail(:to => ["kvirani@span6.com"], :subject => "Span6 Demo Request")
   end
 
   def new_inquiry(inquiry)
     @inquiry = inquiry
-    mail(:to => ["kvirani@span6.com", "tyler@span6.com"], :subject => "Span6 Inquiry")
+    mail(:to => ["kvirani@span6.com"], :subject => "Span6 Inquiry")
   end
 end
